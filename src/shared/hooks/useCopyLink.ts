cfb1d10ae772fb050b202224a219973b007b004b
mintmin <mintmin0320@gmail.com>
2024-08-04 19:06:03 +0900

    feat: URL 카피 hook 공용 컴포넌트 등록

---
+++ src/shared/hooks/useCopyLink.ts
@@ -0,0 +1,29 @@
+import toast from "react-hot-toast"
+
+import ROUTE_PATH from "@/shared/constants/path"
+import { copyURL } from "@/shared/utils/copyURL"
+
+const useCopyLink = () => {
+  const handleCopyButtonClick = () => {
+    copyURL({
+      title: "가챠 공유하기",
+      url: `${ROUTE_PATH.DEV_URL}${ROUTE_PATH.GACHA_DRAW}`,
+      onCompleted: () => {
+        toast.success("친구에게 링크를 공해 보세요!", {
+          duration: 8000,
+        })
+      },
+      onError: () => {
+        toast.error("링크 공유에 실패했어요.", {
+          duration: 8000,
+        })
+      },
+    })
+  }
+
+  return {
+    handleCopyButtonClick,
+  }
+}
+
+export default useCopyLink
