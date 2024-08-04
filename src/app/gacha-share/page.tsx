476be724bc3a0cf4e0099f01484de66ce00510d2
mintmin <mintmin0320@gmail.com>
2024-08-04 19:07:34 +0900

    feat: URL 공유 페이지

---
+++ src/app/gacha-share/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+
+import GachaShareButton from "@/features/gacha-landing-share/ui/GachaShareButton"
+
+const GachaSharePage = () => {
+  return (
+    <main className="flex h-dvh flex-col items-center gap-[8px] bg-white pt-[152px]">
+      <Image
+        src="/icons/mini-gacha-landing.svg"
+        width={208}
+        height={208}
+        alt="mini gacha"
+      />
+      <p className="text-center text-xl font-bold leading-xl tracking-[-0.04px] text-[#202020]">
+        가챠를 공유할 준비가
+        <br /> 모두 끝났어요!
+      </p>
+      <GachaShareButton />
+    </main>
+  )
+}
+
+export default GachaSharePage
---
+++ src/features/gacha-landing-share/ui/GachaShareButton.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import Button from "@/shared/@common/ui/Button"
+
+import useCopyLink from "../../../shared/hooks/useCopyLink"
+
+const GachaShareButton = () => {
+  const { handleCopyButtonClick } = useCopyLink()
+
+  return (
+    <Button
+      type="button"
+      className="fixed bottom-0 left-1/2 mb-[10px] max-w-[calc(100%-32px)] -translate-x-1/2 transform"
+      theme="blue"
+      onClick={handleCopyButtonClick}
+    >
+      가챠 공유하기
+    </Button>
+  )
+}
+
+export default GachaShareButton
