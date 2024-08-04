f8b1a13834e1e094bee65db7594744289e324bd7
mintmin <mintmin0320@gmail.com>
2024-08-04 18:56:48 +0900

    feat: 공용 상수, 스타일 추가

---
+++ src/shared/constants/path.ts
@@ -0,0 +1,20 @@
+/** 페이지 경로
+ *
+ *
+ */
+
+const ROUTE_PATH = {
+  ROOT: "/",
+  DEV_URL: "http://localhost:3000",
+  DISTANCE_SEARCH: "/distance-search",
+  TRANSIT_ROTE: "/transit-route-search",
+  TRANSIT_ROTE_RESULT: "/transit-route-result",
+  GACHA_CREATE: "/gacha-create",
+  GACHA_CREATE_WAIT: "/gacha-create-wait",
+  CHOICE_MESSAGE: "/choice-message",
+  GACHA_SHARE: "/gacha-share",
+  GACHA_DRAW: "/gacha-draw",
+  GACHA_DRAW_LANDING: "/gacha-draw-shupple",
+} as const
+
+export default ROUTE_PATH
