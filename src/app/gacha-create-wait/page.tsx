2050aebca1a22bfeef1bbcc45455f9bb37c13e44
mintmin <mintmin0320@gmail.com>
2024-08-06 22:09:07 +0900

    feat: id params 추가

--- src/app/gacha-create-wait/page.tsx
+++ src/app/gacha-create-wait/page.tsx
@@ -3,9 +3,17 @@ import GachaWaitRouteWrapper from "@/shared/@common/ui/GachaWaitRouteWrapper"
 
 import ROUTE_PATH from "@/shared/@common/constants/path"
 
-const GachaCreateLadingPage = () => {
+interface GachaCreateLadingPageProps {
+  searchParams: Record<string, string>
+}
+
+const GachaCreateLadingPage = ({
+  searchParams,
+}: GachaCreateLadingPageProps) => {
   return (
-    <GachaWaitRouteWrapper path={`${ROUTE_PATH.CHOICE_MESSAGE}?step=4`}>
+    <GachaWaitRouteWrapper
+      path={`${ROUTE_PATH.CHOICE_MESSAGE}?id=${searchParams.id}`}
+    >
       <main className="relative flex h-dvh w-full flex-col items-center bg-white">
         <GachaWait text="감사 가챠를 등록하는 중이에요!" />
       </main>
--- src/features/provincial-route-result/ui/NavigationButton.tsx
+++ src/features/provincial-route-result/ui/NavigationButton.tsx
@@ -5,8 +5,10 @@ import { useRouter } from "next/navigation"
 import Button from "@/shared/@common/ui/Button"
 
 import ROUTE_PATH from "@/shared/@common/constants/path"
+import useGetSearchParam from "@/shared/@common/hooks/useGetSearchParams"
 
 const NavigationButton = () => {
+  const userId = useGetSearchParam("id")
   const router = useRouter()
 
   return (
@@ -14,7 +16,7 @@ const NavigationButton = () => {
       type="button"
       theme="blue"
       className="fixed bottom-0 left-1/2 mb-[10px] max-w-[calc(100%-32px)] -translate-x-1/2 transform"
-      onClick={() => router.push(`${ROUTE_PATH.GACHA_CREATE}`)}
+      onClick={() => router.push(`${ROUTE_PATH.GACHA_CREATE}?id=${userId}`)}
     >
       감사 가챠 만들기
     </Button>
