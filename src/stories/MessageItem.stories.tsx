0ef38f343c8bcb4d5af7ec75e241879076e06b84
mintmin <mintmin0320@gmail.com>
2024-08-07 02:01:09 +0900

    feat: 메시지 리스트 아이템 스토리 추가

---
+++ src/stories/MessageItem.stories.tsx
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import MessageItem from "@/features/provincial-choice-message/ui/MessageItem"
+import { fn } from "@storybook/test"
+
+const meta = {
+  title: "provincial/MessageItem",
+  component: MessageItem,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: "375px",
+          listStyleType: "none",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    message: {
+      control: "text",
+      description: "한마디 메시지",
+    },
+    isSelected: {
+      control: "boolean",
+      description: "한마디 메시지 선택 여부",
+    },
+    onClick: {
+      action: "changed",
+      description: "한마디 메시지 선택 변경 이벤트",
+    },
+  },
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<typeof MessageItem>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    message: "먼 길 올라가는데, 교통비 n빵 어떰?",
+    isSelected: false,
+  },
+}
+
+export const SelectedMessageItem: Story = {
+  args: {
+    message: "먼 길 올라가는데, 교통비 n빵 어떰?",
+    isSelected: true,
+  },
+}
