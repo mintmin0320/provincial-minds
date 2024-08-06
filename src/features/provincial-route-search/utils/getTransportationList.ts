9ed271be1c3c3134ac2dfb58d25186ce26eee979
mintmin <mintmin0320@gmail.com>
2024-08-06 21:14:32 +0900

    feat: 타입 -> 대중교통 변환 함수 추가

---
+++ src/features/provincial-route-search/utils/getTransportationList.ts
@@ -0,0 +1,22 @@
+import { TransportationType } from '@/shared/@common/types/transitRoute.type';
+
+export const getTransportationList = (pathType: number): TransportationType[] => {
+  switch (pathType) {
+    case 1:
+      return ["지하철"];
+    case 2:
+      return ["시내버스"];
+    case 3:
+      return ["시내버스", "지하철"];
+    case 11:
+      return ["열차"];
+    case 12:
+      return ["고속버스", "시외버스"];
+    case 13:
+      return ["항공"];
+    case 20:
+      return ["열차", "고속버스"];
+    default:
+      return ["기타"];
+  }
+};
