1e2630689d678bd4a3f43bbca5cb10e9c5b8f2ff
mintmin <mintmin0320@gmail.com>
2024-08-07 16:51:35 +0900

    refactor: xhr -> fetch 대체

--- src/app/api/transit-route/route.ts
+++ src/app/api/transit-route/route.ts
@@ -1,36 +1,28 @@
 import { NextResponse } from 'next/server';
 
-export async function POST(request:Request) {
+export async function POST(request: Request) {
   try {
     const { sx, sy, ex, ey } = await request.json();
 
     const url = `https://api.odsay.com/v1/api/searchPubTransPathT?SX=${sx}&SY=${sy}&EX=${ex}&EY=${ey}&apiKey=${process.env.NEXT_PUBLIC_ODSAY_API_KEY}`;
 
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-      xhr.send();
-
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            try {
-              const data = JSON.parse(xhr.responseText);
-              resolve(NextResponse.json(data));
-            } catch (error) {
-              console.error('Failed to parse response JSON:', xhr.responseText);
-              reject(NextResponse.json({ error: 'Failed to parse response JSON' }, { status: 500 }));
-            }
-          } else {
-            console.error('API request failed with status:', xhr.status);
-            reject(NextResponse.json({ error: 'Failed to fetch public transit route' }, { status: xhr.status }));
-          }
-        }
-      };
+    const response = await fetch(url, {
+      method: 'GET',
     });
+
+    if (!response.ok) {
+      console.error('API request failed with status:', response.status);
+      return NextResponse.json(
+        { error: 'Failed to fetch public transit route' },
+        { status: response.status }
+      );
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
 
   } catch (error) {
     console.error('Server-side error:', error);
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
 }
