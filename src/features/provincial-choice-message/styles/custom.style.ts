222e2365817bda99b03caf30768282dad223a84c
mintmin <mintmin0320@gmail.com>
2024-08-08 18:52:18 +0900

    style: 파일, 폴더명 변경

--- src/features/provincial-choice-message/styles/custom.style.ts
+++
@@ -1,2 +0,0 @@
-export const customStyle =
-  "flex w-full rounded-[10px] px-[16px] py-[20px] font-semibold tracking-[-0.08px] items-center"
--- src/shared/@common/types/Area.type.ts
+++
@@ -1,4 +0,0 @@
-export interface IAreaProps {
-  urbanArea: string | null
-  provincialArea: string | null
-}
