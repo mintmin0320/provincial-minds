3db7ab0a49e418b94ac12ee405736344503b5ff1
mintmin <mintmin0320@gmail.com>
2024-08-08 19:53:12 +0900

    refactor: 출발, 목적지 입력 페이지 컴포넌트 분리

--- src/features/provincial-route-search/ui/TransitRouteSection.tsx
+++ src/features/provincial-route-search/ui/TransitRouteSection.tsx
@@ -1,25 +1,56 @@
 "use client"
 
-import SearchAreaFieldGroup from "@/shared/@common/ui/AreaSearchFieldGroup"
-import TransitRouteList from "./TransitRouteList"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
-import useGetSearchParam from "@/shared/@common/hooks/useGetSearchParams"
+import SearchAreaFieldGroup from "@/shared/@common/ui/AreaSearchFieldGroup"
+import Button from "@/shared/@common/ui/Button"
+import Loading from "@/shared/@common/ui/Loading"
+import TransitRouteCard from "./TransitRouteCard"
+
+import ROUTE_PATH from "@/shared/@common/constants/path"
+import { fixButtonStyle } from "@/shared/@common/styles/fixButton"
 import { useGetTransitList } from "@/shared/provincial/api/queries/useGetTransitList"
 
 const TransitRouteSection = () => {
-  const userId = useGetSearchParam("id") || null
+  const router = useRouter()
 
-  const { transitList } = useGetTransitList(Number(userId))
+  const { transitList, isLoading, isError } = useGetTransitList()
+
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null)
 
   const areaState = {
-    provincialArea: transitList?.startArea || "",
-    urbanArea: transitList?.endArea || "",
+    provincialArea: transitList?.startArea ?? "",
+    urbanArea: transitList?.endArea ?? "",
   }
+
+  if (isLoading) return <Loading />
+  if (isError) return <div>에러가 발생했어요. 다시 시도해 주세요.</div>
 
   return (
     <section>
+      {/** 출발, 도착지 정보 */}
       <SearchAreaFieldGroup areaState={areaState} type="view" />
-      <TransitRouteList transits={transitList?.transits || []} />
+      <ul className="mt-[37px] flex flex-col gap-[8px] pb-[96px]">
+        {transitList?.transits.map((item, index) => (
+          <div key={item.id} onClick={() => setSelectedIndex(index)}>
+            <TransitRouteCard isSelected={selectedIndex === index} {...item} />
+          </div>
+        ))}
+      </ul>
+      <Button
+        type="button"
+        className={fixButtonStyle}
+        theme="blue"
+        disabled={selectedIndex === null}
+        onClick={() =>
+          router.push(
+            `${ROUTE_PATH.TRANSIT_ROTE_RESULT}?transitId=${selectedIndex}`,
+          )
+        }
+      >
+        여정 계산하기
+      </Button>
     </section>
   )
 }
