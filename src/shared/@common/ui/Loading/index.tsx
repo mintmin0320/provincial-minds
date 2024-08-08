2d09a0c7fe0b5df851795cad6986b27d89d4bda4
mintmin <mintmin0320@gmail.com>
2024-08-08 19:04:37 +0900

    feat: Loading UI 컴포넌트

---
+++ src/shared/@common/ui/Loading/index.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import Image from "next/image"
+
+import { LocalMindsTitleFont } from "../../assets/fonts"
+import { cn } from "../../utils/twMerge"
+
+const Loading = () => {
+  const images = ["city-bus", "intercity-bus", "subway"]
+  const randomImage = images[Math.floor(Math.random() * images.length)]
+
+  return (
+    <div className="flex h-dvh flex-col items-center pt-[140px]">
+      <Image
+        src={`/icons/${randomImage}.svg`}
+        width={208}
+        height={208}
+        alt="city-bus"
+        className="animate-pulse-grow"
+        priority
+      />
+      <span
+        className={cn(LocalMindsTitleFont.className, "text-[22px] font-bold")}
+      >
+        최적의 경로를 찾고있어요 🔍
+      </span>
+    </div>
+  )
+}
+
+export default Loading
