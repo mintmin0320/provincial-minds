e9b0618a4277432c0111a4584deac789491a0e18
mintmin <mintmin0320@gmail.com>
2024-08-20 18:06:03 +0900

    feat: 한마디 메시지 확인 버튼 추가

--- src/shared/@common/ui/GachaVisual/index.tsx
+++ src/shared/@common/ui/GachaVisual/index.tsx
@@ -4,10 +4,15 @@ import Image from "next/image"
 
 import { useGetUserData } from "@/shared/urban/api/queries/useGetUserData"
 
+import { useModals } from "../../hooks/useModals"
+import { cn } from "../../utils/twMerge"
+
 import Capsule from "../Capsule"
+import GachaModal from "../GachaModal"
 
 const GachaVisual = ({ isCreateGacha }: { isCreateGacha: boolean }) => {
   const { userData } = useGetUserData()
+  const { open } = useModals()
 
   return (
     <section className="relative mt-[12px] h-[650px] w-full">
@@ -35,6 +40,27 @@ const GachaVisual = ({ isCreateGacha }: { isCreateGacha: boolean }) => {
         isCreateGacha={isCreateGacha}
         capsuleText={userData?.gachaMessage ?? ""}
       />
+      {!isCreateGacha && (
+        <button
+          className={cn(
+            "absolute right-[24%] top-[2%] flex h-[40px] w-[40px] items-center justify-center rounded-full bg-[#F8D169]",
+            "mo:right-[8%] mo:top-[2%]",
+          )}
+          onClick={() =>
+            open(GachaModal, {
+              isSendMessage: true,
+              customMessage: userData?.userMessage ?? "",
+            })
+          }
+        >
+          <Image
+            src="/icons/message.svg"
+            alt="message-open-button"
+            width={28}
+            height={28}
+          />
+        </button>
+      )}
     </section>
   )
 }
