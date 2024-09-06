10563da74f5d79f954c1ce9a419b2e85a90786e5
mintmin <mintmin0320@gmail.com>
2024-09-06 18:45:33 +0900

    feat: css preload 방식 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import ModalContainer from "@/shared/@common/ui/ModalContainer"
 import RQProvider from "@/shared/@common/ui/RQProvider"
 import siteMetadata from "@/shared/siteMetadata"
 import { CookiesProvider } from "next-client-cookies/server"
+import Head from "next/head"
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
@@ -44,8 +45,51 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const handleLoad = (e: React.SyntheticEvent<HTMLLinkElement, Event>) => {
+    const target = e.target as HTMLLinkElement
+    target.media = "all"
+  }
+
   return (
     <html lang={siteMetadata.language} className={LocalMindsBodyFont.className}>
+      <Head>
+        {/* CSS 파일 미리 로드 */}
+        <link
+          rel="preload"
+          href="/_next/static/css/bd28c9cf57fafd45.css"
+          as="style"
+        />
+        <link
+          rel="preload"
+          href="/_next/static/css/2c3723fa6751881f.css"
+          as="style"
+        />
+        <link
+          rel="preload"
+          href="/_next/static/css/d8b74ee1af12be30.css"
+          as="style"
+        />
+
+        {/* 비동기 CSS 로드 */}
+        <link
+          rel="stylesheet"
+          href="/_next/static/css/bd28c9cf57fafd45.css"
+          media="print"
+          onLoad={handleLoad} // 문자열 대신 함수로 변경
+        />
+        <link
+          rel="stylesheet"
+          href="/_next/static/css/2c3723fa6751881f.css"
+          media="print"
+          onLoad={handleLoad} // 문자열 대신 함수로 변경
+        />
+        <link
+          rel="stylesheet"
+          href="/_next/static/css/d8b74ee1af12be30.css"
+          media="print"
+          onLoad={handleLoad} // 문자열 대신 함수로 변경
+        />
+      </Head>
       <body className="mx-auto h-full max-w-[767px] bg-white">
         <CookiesProvider>
           <RQProvider>{children}</RQProvider>
