0e2ee61fa00cfb44bc96dbcdc196d8dc9685df36
mintmin <mintmin0320@gmail.com>
2024-09-12 22:12:11 +0900

    refactor: 가챠 메시지 생성 로직 변경(mutation -> api 함수 호출 )

--- src/shared/provincial/api/mutations/useSetGachaMessage.ts
+++ src/shared/provincial/api/mutations/useSetGachaMessage.ts
@@ -1,27 +1,12 @@
-import { useMutation } from '@tanstack/react-query'
-import { useCookies } from 'next-client-cookies'
-import toast from 'react-hot-toast'
 
 import { updateGachaMessage } from '@/actions/user'
 
-async function postGachaMessage(userId: number, gachaMessage: string): Promise<boolean> {
+export async function postGachaMessage(userId: number, gachaMessage: string): Promise<boolean> {
   const result = await updateGachaMessage({ id: userId, gachaMessage })
 
   if (!result) {
-    throw new Error('Failed to update gacha message')
+    throw new Error('가챠 메시지 생성에 실패했습니다.')
   }
 
   return result
-}
-
-export default function useSetGachaMessage() {
-  const cookies = useCookies()
-  const userId = cookies.get("userId")
-
-  return useMutation<boolean, Error, { gachaMessage: string }>({
-    mutationFn: ({ gachaMessage }) => postGachaMessage(Number(userId), gachaMessage),
-    onError: (error) => {
-      toast.error(error.message)
-    },
-  })
 }
