1589bd75e794e8d4a3fc263aa212262917773a66
mintmin <mintmin0320@gmail.com>
2024-09-15 01:51:29 +0900

    style: 함수 사용 간편화

--- src/shared/@common/ui/CreateGachaButton/index.tsx
+++ src/shared/@common/ui/CreateGachaButton/index.tsx
@@ -12,15 +12,11 @@ const CreateGachaButton = () => {
   const { open } = useModals()
   const gachaMessage = useGachaStore((state) => state.gachaMessage)
 
-  const openModal = () => {
-    open(CreateGachaModal)
-  }
-
   return (
     <button
       type="button"
       className="absolute left-[51%] top-[40%] -translate-x-[50%] -translate-y-[20px] transform"
-      onClick={openModal}
+      onClick={() => open(CreateGachaModal)}
     >
       {gachaMessage ? (
         <div className="h-[42px] w-[114px] rotate-[6.1deg] content-center bg-white text-center text-sm font-bold tracking-[-0.28px] text-mint01">
