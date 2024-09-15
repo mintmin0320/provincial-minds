3dafd45897a4480e7b5bf7b76888c77fe7da92b8
mintmin <mintmin0320@gmail.com>
2024-09-15 19:09:36 +0900

    refactor: 요금, 시간별 메시지 수정 및 추가

--- src/shared/@common/utils/getAnalogyMessage.ts
+++ src/shared/@common/utils/getAnalogyMessage.ts
@@ -8,11 +8,11 @@ interface IAnalogyMessageTypes {
 const getTimeAnalogyMessage = (hour: number, minute: number) => {
   if (hour >= 10) {
     return `${hour}시간이면 거의 비행기로 미국을 갈 수 있는`
-  } else if (hour > 5) {
+  } else if (hour >= 5) {
     return `${hour}시간이면 1일 평균 근로`
-  } else if (hour > 2) {
+  } else if (hour >= 2) {
     return `${hour}시간이면 어벤져스 엔드게임 한 편 볼 수 있는`
-  } else if (hour > 1) {
+  } else if (hour >= 1) {
     return `${hour}시간이면 축구 한 경기를 볼 수 있는`
   } else if (hour === 0 && minute > 0) {
     return `${minute}분이면 간단한 유튜브 동영상을 볼 수 있는`
@@ -24,15 +24,22 @@ const getTimeAnalogyMessage = (hour: number, minute: number) => {
 }
 
 const getCostAnalogyMessage = (payment: number) => {
-  if (payment > 30000) {
+  if (payment >= 30000) {
     return `${payment.toLocaleString()}원이면 탕후루 30개`
-  } else if (payment > 23000) {
+  } else if (payment >= 23000) {
     return `${payment.toLocaleString()}원이면 황금올리브치킨 한 마리`
-  } else if (payment > 15000) {
+  } else if (payment >= 15000) {
     return `${payment.toLocaleString()}원이면 영화관 티켓 + 팝콘 + 콜라`
-  } else if (payment > 10000) {
+  } else if (payment >= 10000) {
     return `${payment.toLocaleString()}원이면 넷플릭스 결제 비용`
-  } else if (payment > 500) {
+  }
+  else if (payment <= 6000) {
+    return `${payment.toLocaleString()}원이면 카페 음료 한 잔`
+  } 
+  else if (payment <= 3000) {
+    return `${payment.toLocaleString()}원이면 간단한 간식`
+  } 
+  else if (payment <= 1000) {
     return `${payment.toLocaleString()}원이면 불량식품도 못 사는`
   } else if (payment === 0) {
     return `0원이면 그냥 서로 더치페이 해도 될`
